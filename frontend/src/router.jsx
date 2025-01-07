2faa016a27e3ce481ce0c466d9903fa42e1246db
cookiee011 <lovelydahee0503@naver.com>
2025-01-07 12:55:59 +0900

    라우팅 설정

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,11 +1,17 @@
-import { createBrowserRouter } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import MainPage2 from "./pages/MainPage2";
 
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/summary" element={<MainPage2 />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export const router = createBrowserRouter([
-    {
-        path: "/Main",
-        element: <MainPage />,
-        errorElement: <NotFound />,
-    }
-]);
+export default AppRouter;
